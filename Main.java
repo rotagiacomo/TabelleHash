413d8146a23d29cc85c7ae9f81832183d65098f0
rotagiacomo <rota.giacomo.studente@itispaleocapa.it>
2026-03-20 11:38:42 +0000

    aggiunta metodo add

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main{
     public static void main(){
         TabellaHash tHash = new TabellaHash(10);
+        tHash.add("casa", "12");
+        tHash.add("caas", "199");
+        tHash.add("ava", "569");
         System.out.println(tHash);
     }
 }
